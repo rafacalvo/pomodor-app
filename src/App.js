4a4e61b2743698002dd1261c3c862695636e295d
rafacalvo <rafael.calvo.gonzalez@gmail.com>
2021-03-11 18:56:06 -0400

    Added more text to the instructions

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className='container text-center flex flex-col justify-center items-center p-5'>
       <h1 className='text-4xl md:text-5xl font-bold m-6'>Your Pomodoro App!</h1>
       <img className='mb-7' src={logo} alt={'Pomodoro logo'} width='120' />
-      <p className='text-md mb-2'>
+      <p className='text-md mb-4'>
         This timer is based on the{' '}
         <a
           className='text-red-500'
@@ -21,9 +21,9 @@ function App() {
         intervals (also called sessions).
       </p>
       <p className='text-lg font-semibold'>
-        To start using this app hit the play button. <br /> You can hit the{' '}
-        <i class='fas fa-plus mx-1'></i> and <i class='fas fa-minus mx-1'></i>{' '}
-        buttons to change your times.
+        INSTRUCTIONS: To start using this app hit the play button. <br /> You
+        can hit the <i class='fas fa-plus mx-1'></i> and{' '}
+        <i class='fas fa-minus mx-1'></i> buttons to change your times.
       </p>
       <Customizer>
         <Timer />
